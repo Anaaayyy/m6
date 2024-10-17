67ba5da618d8193fa97ce25cef694b3e87a4c8cf
Anaaayyy <anna.bukshtynova251106@gmail.com>
2024-10-17 13:17:40 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace m6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.задачиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Описание = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Дата = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Статус = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.btn_Clear = new System.Windows.Forms.Button();
+            this.btn_Update = new System.Windows.Forms.Button();
+            this.btn_Install = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.задачиToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(985, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // задачиToolStripMenuItem
+            // 
+            this.задачиToolStripMenuItem.Name = "задачиToolStripMenuItem";
+            this.задачиToolStripMenuItem.Size = new System.Drawing.Size(72, 24);
+            this.задачиToolStripMenuItem.Text = "Задачи";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(67, 24);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.Описание,
+            this.Дата,
+            this.Статус});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 31);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(707, 525);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // id
+            // 
+            this.id.HeaderText = "id";
+            this.id.MinimumWidth = 6;
+            this.id.Name = "id";
+            this.id.Width = 125;
+            // 
+            // Описание
+            // 
+            this.Описание.HeaderText = "Описание";
+            this.Описание.MinimumWidth = 6;
+            this.Описание.Name = "Описание";
+            this.Описание.Width = 125;
+            // 
+            // Дата
+            // 
+            this.Дата.HeaderText = "Дата";
+            this.Дата.MinimumWidth = 6;
+            this.Дата.Name = "Дата";
+            this.Дата.Width = 125;
+            // 
+            // Статус
+            // 
+            this.Статус.HeaderText = "Статус";
+            this.Статус.MinimumWidth = 6;
+            this.Статус.Name = "Статус";
+            this.Статус.Width = 125;
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_Add.Location = new System.Drawing.Point(799, 146);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(153, 55);
+            this.btn_Add.TabIndex = 2;
+            this.btn_Add.Text = "Добавить";
+            this.btn_Add.UseVisualStyleBackColor = true;
+            this.btn_Add.Click += new System.EventHandler(this.btn_Add_Click);
+            // 
+            // btn_Clear
+            // 
+            this.btn_Clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_Clear.Location = new System.Drawing.Point(799, 314);
+            this.btn_Clear.Name = "btn_Clear";
+            this.btn_Clear.Size = new System.Drawing.Size(153, 55);
+            this.btn_Clear.TabIndex = 3;
+            this.btn_Clear.Text = "Удалить";
+            this.btn_Clear.UseVisualStyleBackColor = true;
+            this.btn_Clear.Click += new System.EventHandler(this.btn_Clear_Click);
+            // 
+            // btn_Update
+            // 
+            this.btn_Update.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_Update.Location = new System.Drawing.Point(799, 230);
+            this.btn_Update.Name = "btn_Update";
+            this.btn_Update.Size = new System.Drawing.Size(153, 55);
+            this.btn_Update.TabIndex = 4;
+            this.btn_Update.Text = "Обновить";
+            this.btn_Update.UseVisualStyleBackColor = true;
+            this.btn_Update.Click += new System.EventHandler(this.btn_Update_Click);
+            // 
+            // btn_Install
+            // 
+            this.btn_Install.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_Install.Location = new System.Drawing.Point(799, 64);
+            this.btn_Install.Name = "btn_Install";
+            this.btn_Install.Size = new System.Drawing.Size(153, 55);
+            this.btn_Install.TabIndex = 5;
+            this.btn_Install.Text = "Загрузить";
+            this.btn_Install.Click += new System.EventHandler(this.btn_Install_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(985, 612);
+            this.Controls.Add(this.btn_Install);
+            this.Controls.Add(this.btn_Update);
+            this.Controls.Add(this.btn_Clear);
+            this.Controls.Add(this.btn_Add);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem задачиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Описание;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Дата;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Статус;
+        private System.Windows.Forms.Button btn_Add;
+        private System.Windows.Forms.Button btn_Clear;
+        private System.Windows.Forms.Button btn_Update;
+        private System.Windows.Forms.Button btn_Install;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions; // Добавляем пространство имен для работы с регулярными выражениями
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace m6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btn_Install_Click(object sender, EventArgs e)
+        {
+            // Создаем соединение
+            string connectionString = "provider=Microsoft.Jet.OLEDB.4.0;Data Source=Database.mdb";
+            OleDbConnection dbConnection = new OleDbConnection(connectionString); // создаем соединение
+
+            // Выполняем запрос к БД
+            dbConnection.Open();
+            string query = "SELECT * FROM table1";
+            OleDbCommand dbCommand = new OleDbCommand(query, dbConnection);
+            OleDbDataReader dbReader = dbCommand.ExecuteReader();
+
+            // Проверка данных
+            if (dbReader.HasRows == false)
+            {
+                MessageBox.Show("Данные не найдены", "Ошибка!");
+            }
+            else
+            {
+                while (dbReader.Read())
+                {
+                    dataGridView1.Rows.Add(dbReader["id"], dbReader["description"], dbReader["d"], dbReader["state"]);
+                }
+            }
+
+            // Закрываем соединение
+            dbReader.Close();
+            dbConnection.Close();
+        }
+
+        private bool IsDateValid(string date)
+        {
+            // Проверяем дату с помощью регулярного выражения в формате "dd.mm.yyyy"
+            Regex regex = new Regex(@"^\d{2}\.\d{2}\.\d{4}$");
+            return regex.IsMatch(date);
+        }
+
+        private void btn_Add_Click(object sender, EventArgs e)
+        {
+            // Проверка количества выбранных строк
+            if (dataGridView1.SelectedRows.Count != 1)
+            {
+                MessageBox.Show("Выберите одну строку!", "Внимание!");
+                return;
+            }
+
+            // Заполним выбранную строку
+            int index = dataGridView1.SelectedRows[0].Index;
+
+            // Проверка на данные в таблице
+            if (dataGridView1.Rows[index].Cells[0].Value == null ||
+                dataGridView1.Rows[index].Cells[1].Value == null ||
+                dataGridView1.Rows[index].Cells[2].Value == null ||
+                dataGridView1.Rows[index].Cells[3].Value == null)
+            {
+                MessageBox.Show("Не все данные введены!", "Внимание!");
+                return;
+            }
+
+            // Считываем данные
+            string id = dataGridView1.Rows[index].Cells[0].Value.ToString();
+            string description = dataGridView1.Rows[index].Cells[1].Value.ToString();
+            string d = dataGridView1.Rows[index].Cells[2].Value.ToString();
+            string state = dataGridView1.Rows[index].Cells[3].Value.ToString();
+
+            // Проверка формата даты
+            if (!IsDateValid(d))
+            {
+                MessageBox.Show("Дата должна быть введена в формате dd.mm.yyyy", "Ошибка!");
+                return;
+            }
+
+            // Создаем соединение
+            string connectionString = "provider=Microsoft.Jet.OLEDB.4.0;Data Source=Database.mdb";
+            OleDbConnection dbConnection = new OleDbConnection(connectionString); // создаем соединение
+
+            // Выполняем запрос к БД
+            dbConnection.Open();
+            string query = "INSERT INTO table1 VALUES (" + id + ", '" + description + "', '" + d + "', '" + state + "')";
+            OleDbCommand dbCommand = new OleDbCommand(query, dbConnection);
+
+            // Выполняем запрос
+            if (dbCommand.ExecuteNonQuery() != 1)
+                MessageBox.Show("Ошибка выполнения запроса!", "Ошибка!");
+            else
+                MessageBox.Show("Данные добавлены", "Внимание!");
+
+            // Закрываем соединение с БД
+            dbConnection.Close();
+        }
+
+        private void btn_Update_Click(object sender, EventArgs e)
+        {
+            // Проверка количества выбранных строк
+            if (dataGridView1.SelectedRows.Count != 1)
+            {
+                MessageBox.Show("Выберите одну строку!", "Внимание!");
+                return;
+            }
+
+            // Заполним выбранную строку
+            int index = dataGridView1.SelectedRows[0].Index;
+
+            // Проверка на данные в таблице
+            if (dataGridView1.Rows[index].Cells[0].Value == null ||
+                dataGridView1.Rows[index].Cells[1].Value == null ||
+                dataGridView1.Rows[index].Cells[2].Value == null ||
+                dataGridView1.Rows[index].Cells[3].Value == null)
+            {
+                MessageBox.Show("Не все данные введены!", "Внимание!");
+                return;
+            }
+
+            // Считываем данные
+            string id = dataGridView1.Rows[index].Cells[0].Value.ToString();
+            string description = dataGridView1.Rows[index].Cells[1].Value.ToString();
+            string d = dataGridView1.Rows[index].Cells[2].Value.ToString();
+            string state = dataGridView1.Rows[index].Cells[3].Value.ToString();
+
+            // Проверка формата даты
+            if (!IsDateValid(d))
+            {
+                MessageBox.Show("Дата должна быть введена в формате dd.mm.yyyy", "Ошибка!");
+                return;
+            }
+
+            // Создаем соединение
+            string connectionString = "provider=Microsoft.Jet.OLEDB.4.0;Data Source=Database.mdb";
+            OleDbConnection dbConnection = new OleDbConnection(connectionString); // создаем соединение
+
+            // Выполняем запрос к БД
+            dbConnection.Open();
+            string query = "UPDATE table1 SET Description ='" + description + "', D = '" + d + "', State = '" + state + "' WHERE id = " + id;
+            OleDbCommand dbCommand = new OleDbCommand(query, dbConnection);
+
+            // Выполняем запрос
+            if (dbCommand.ExecuteNonQuery() != 1)
+                MessageBox.Show("Ошибка выполнения запроса!", "Ошибка!");
+            else
+            {
+                MessageBox.Show("Данные изменены", "Внимание!");
+            }
+
+            // Закрываем соединение с БД
+            dbConnection.Close();
+        }
+
+        private void btn_Clear_Click(object sender, EventArgs e)
+        {
+            //проверка количества выбранных строк
+            if (dataGridView1.SelectedRows.Count != 1)
+            {
+                MessageBox.Show("Выберите одну строку!", "Внимение!");
+                return;
+            }
+
+            //Заполним выбранную строку
+            int index = dataGridView1.SelectedRows[0].Index;
+
+            //Проверка на данные в таблице
+            if (dataGridView1.Rows[index].Cells[0].Value == null)
+            {
+                MessageBox.Show("Не все данные введены!", "Внимение!");
+                return;
+            }
+
+            //Считываем данные
+            string id = dataGridView1.Rows[index].Cells[0].Value.ToString();
+            
+            //Создаем соединение
+            string connectionString = "provider=Microsoft.Jet.OLEDB.4.0;Data Source=Database.mdb";//строка соединения
+            OleDbConnection dbConnection = new OleDbConnection(connectionString); //создаем соединение
+
+            //Выполняем запрос к БД
+            dbConnection.Open();
+            string query = "DELETE FROM table1 WHERE id =" + id;
+            OleDbCommand dbCommand = new OleDbCommand(query, dbConnection);
+
+            //Выполняем запрос
+            if (dbCommand.ExecuteNonQuery() != 1)
+                MessageBox.Show("Ошибка выполнения запроса!", "Ошибка!");
+            else
+            {
+                MessageBox.Show("Данные удалены", "Внимание!");
+                //Удаляем данные из таблицы 
+                dataGridView1.Rows.RemoveAt(index);
+
+            }
+
+            //Закрываем соединение с БД
+            dbConnection.Close();
+        }
+    }
+}
